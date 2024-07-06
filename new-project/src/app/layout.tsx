2b0cd1e8886db3fdc43546a719afbce2cc851c2b
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2024-07-06 23:36:59 +0000

    tab icon fevicon changed

--- new-project/src/app/layout.tsx
+++ new-project/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
@@ -15,14 +16,21 @@ interface RootLayoutProps {
   children: React.ReactNode;
 }
 
+const LoadingSpinner = () => (
+  <div className="flex items-center justify-center h-screen">
+    <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-gray-900"></div>
+  </div>
+);
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={`${inter.className} flex flex-col min-h-screen`}>
         <div className="flex-grow flex flex-col">
-          {" "}
           <Navbar />
-          <main>{children}</main>
+          <main>
+            <Suspense fallback={<LoadingSpinner />}>{children}</Suspense>
+          </main>
         </div>
         <Footer />
       </body>
