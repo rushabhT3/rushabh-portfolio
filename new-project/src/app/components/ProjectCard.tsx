5f06a139cf7ec7fbab992d5472b2684bc5b0dce6
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2024-07-07 13:25:25 +0000

    theme try

--- new-project/src/app/components/ProjectCard.tsx
+++ new-project/src/app/components/ProjectCard.tsx
@@ -15,12 +15,15 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
 }) => {
   return (
     <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md mb-4 mx-4 mt-4">
-      <h2 className="text-2xl font-bold mb-2 dark:text-black">{title}</h2>
-      <p className="mb-2 dark:text-gray-300">{description}</p>
-      <p className="text-sm text-gray-500 mb-2 dark:text-gray-400">
+      <h2 className="text-2xl font-bold mb-2 text-google-gray-800">{title}</h2>
+      <p className="mb-2 text-google-gray-600">{description}</p>
+      <p className="text-sm text-gray-500 mb-2 text-google-gray-400">
         <strong>Tech Used:</strong> {tech}
       </p>
-      <a href={link} className="text-blue-500 dark:text-blue-400 hover:underline">
+      <a
+        href={link}
+        className="text-blue-500 dark:text-blue-400 hover:underline"
+      >
         View Project on GitHub 🔗
       </a>
     </div>
