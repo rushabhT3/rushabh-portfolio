ffdd8b797a751b1dbadb0d0b94fd8de034c3965e
Rushabh Trivedi <41021094+rushabhT3@users.noreply.github.com>
2024-09-16 05:59:22 +0530

    Update page.tsx

--- new-project/src/app/projects/page.tsx
+++ new-project/src/app/projects/page.tsx
@@ -2,7 +2,9 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 import ProjectCard from "../components/ProjectCard";
+import projectsData from './projects.json';
 
 type Project = {
   title: string;
@@ -11,64 +13,76 @@ type Project = {
   link: string;
 };
 
-const projects: Project[] = [
-  {
-    title: "Full Stack Mailbox Client",
-    description:
-      "A comprehensive email management system with secure authentication and database encryption, featuring a sleek user interface built with React and Tailwind CSS. Includes JWT authentication for enhanced security and performance.",
-    tech: "React.js, Express.js, MySQL, Sequelize, Tailwind CSS, React Router, JWT",
-    link: "https://github.com/rushabhT3/mailBoxClientReact",
-  },
-  {
-    title: "Real-Time Group Chat Application",
-    description:
-      "A dynamic group chat application that supports real-time messaging using Socket.io. Secure user authentication and data management are ensured with MySQL and AWS. Features include user login, message encryption, and group chat functionalities.",
-    tech: "Express.js, MySQL, Socket.io, bcrypt, cors, dotenv, multer, mysql2, node-cron, pm2, sequelize, aws-sdk",
-    link: "https://github.com/rushabhT3/ChatApp",
-  },
-  {
-    title: "Backend Expense Tracker",
-    description:
-      "A robust backend for an expense tracking application. Implements secure user authentication with JWT and data encryption. Utilizes AWS for deployment and data storage. Designed to handle various expense tracking operations efficiently.",
-    tech: "Express.js, SQL, AWS, JWT, Bcrypt, Sequelize, Brevo",
-    link: "https://github.com/rushabhT3/node.js/tree/main/Express.Js/AWSapp",
-  },
-  {
-    title: "Multi-Column-To-Do-App with Drag and Drop Functionality",
-    description:
-    "A dynamic Todo application built with Next.js and MongoDB, featuring a drag-and-drop interface. Utilizes Tailwind CSS for a responsive design, shadcn/ui for enhanced interactions, and Lucide React for iconic visuals. Allows users to add, edit, and delete tasks, with the added functionality of dragging and dropping tasks to reorder them.",
-    tech: "TypeScript, Next.js, MongoDB, @hello-pangea/dnd, Tailwind CSS, shadcn/ui, Lucide React",
-    link: "https://github.com/rushabhT3/Multi-Column-To-Do-App",
-  },
-  {
-    title: "Frontend Expense Tracker",
-    description:
-      "A serverless expense tracker application powered by Google Firebase for the backend and React.js for the frontend. Features include user authentication, real-time data updates, and an intuitive user interface for tracking expenses.",
-    tech: "React.js, React Router, Firebase",
-    link: "https://github.com/rushabhT3/react/tree/main/expense-tracker",
-  },
-  {
-    title: "Mongoose Expense Tracker",
-    description:
-      "A robust backend for an expense tracking application powered by Mongoose. Implements secure user authentication with JWT and data encryption. Utilizes AWS for deployment and data storage. Designed to handle various expense tracking operations efficiently.",
-    tech: "Express.js, AWS, JWT, Bcrypt, Mongoose (MongoDB), Brevo",
-    link: "https://github.com/rushabhT3/mongoose-Expense-Tracker",
-  },
-  {
-    title: "Portfolio Website",
-    description:
-      "A modern and responsive portfolio website showcasing my projects and skills. Built using Next.js and TypeScript for a robust and scalable frontend, and styled with Tailwind CSS for a clean and adaptive design. Deployed and hosted on Vercel for seamless performance and accessibility.",
-    tech: "Next.js, TypeScript, Tailwind CSS, react-icons, framer-motion, lucide-react",
-    link: "https://github.com/rushabhT3/rushabh-portfolio/tree/main/new-project",
-  },
-];
+// const projects: Project[] = [
+//   {
+//     title: "Full Stack Mailbox Client",
+//     description:
+//       "A comprehensive email management system with secure authentication and database encryption, featuring a sleek user interface built with React and Tailwind CSS. Includes JWT authentication for enhanced security and performance.",
+//     tech: "React.js, Express.js, MySQL, Sequelize, Tailwind CSS, React Router, JWT",
+//     link: "https://github.com/rushabhT3/mailBoxClientReact",
+//   },
+//   {
+//     title: "Real-Time Group Chat Application",
+//     description:
+//       "A dynamic group chat application that supports real-time messaging using Socket.io. Secure user authentication and data management are ensured with MySQL and AWS. Features include user login, message encryption, and group chat functionalities.",
+//     tech: "Express.js, MySQL, Socket.io, bcrypt, cors, dotenv, multer, mysql2, node-cron, pm2, sequelize, aws-sdk",
+//     link: "https://github.com/rushabhT3/ChatApp",
+//   },
+//   {
+//     title: "Backend Expense Tracker",
+//     description:
+//       "A robust backend for an expense tracking application. Implements secure user authentication with JWT and data encryption. Utilizes AWS for deployment and data storage. Designed to handle various expense tracking operations efficiently.",
+//     tech: "Express.js, SQL, AWS, JWT, Bcrypt, Sequelize, Brevo",
+//     link: "https://github.com/rushabhT3/node.js/tree/main/Express.Js/AWSapp",
+//   },
+//   {
+//     title: "Multi-Column-To-Do-App with Drag and Drop Functionality",
+//     description:
+//     "A dynamic Todo application built with Next.js and MongoDB, featuring a drag-and-drop interface. Utilizes Tailwind CSS for a responsive design, shadcn/ui for enhanced interactions, and Lucide React for iconic visuals. Allows users to add, edit, and delete tasks, with the added functionality of dragging and dropping tasks to reorder them.",
+//     tech: "TypeScript, Next.js, MongoDB, @hello-pangea/dnd, Tailwind CSS, shadcn/ui, Lucide React",
+//     link: "https://github.com/rushabhT3/Multi-Column-To-Do-App",
+//   },
+//   {
+//     title: "Frontend Expense Tracker",
+//     description:
+//       "A serverless expense tracker application powered by Google Firebase for the backend and React.js for the frontend. Features include user authentication, real-time data updates, and an intuitive user interface for tracking expenses.",
+//     tech: "React.js, React Router, Firebase",
+//     link: "https://github.com/rushabhT3/react/tree/main/expense-tracker",
+//   },
+//   {
+//     title: "Mongoose Expense Tracker",
+//     description:
+//       "A robust backend for an expense tracking application powered by Mongoose. Implements secure user authentication with JWT and data encryption. Utilizes AWS for deployment and data storage. Designed to handle various expense tracking operations efficiently.",
+//     tech: "Express.js, AWS, JWT, Bcrypt, Mongoose (MongoDB), Brevo",
+//     link: "https://github.com/rushabhT3/mongoose-Expense-Tracker",
+//   },
+//   {
+//     title: "Portfolio Website",
+//     description:
+//       "A modern and responsive portfolio website showcasing my projects and skills. Built using Next.js and TypeScript for a robust and scalable frontend, and styled with Tailwind CSS for a clean and adaptive design. Deployed and hosted on Vercel for seamless performance and accessibility.",
+//     tech: "Next.js, TypeScript, Tailwind CSS, react-icons, framer-motion, lucide-react",
+//     link: "https://github.com/rushabhT3/rushabh-portfolio/tree/main/new-project",
+//   },
+// ];
 
 const ITEMS_PER_PAGE = 4;
 
 const ProjectList: React.FC = () => {
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [darkMode, setDarkMode] = useState(false);
 
+  useEffect(() => {
+    // Simulate an async operation to mimic data fetching
+    const loadProjects = async () => {
+      setProjects(projectsData);
+      setIsLoading(false);
+    };
+
+    loadProjects();
+  }, []);
+  
   useEffect(() => {
     const prefersDarkMode = window.matchMedia(
       "(prefers-color-scheme: dark)"
